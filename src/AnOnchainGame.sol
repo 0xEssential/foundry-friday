7199c9d04a7be6d040cdd9bf0ec56e77f3a1050c
Sam Bauch <sbauch@gmail.com>
2022-06-29 22:33:26 -0400

    fun stuff

--- src/AnOnchainGame.sol
+++ src/AnOnchainGame.sol
@@ -5,26 +5,65 @@ import "essential-contracts/contracts/fwd/EssentialERC2771Context.sol";
 
 contract AnOnchainGame is EssentialERC2771Context {
   // Constants
-  uint256 public constant teamCount = 3;
-
+  uint8 public constant teamCount = 3;
+  uint256 public constant turnLength = 12 hours;
+  
   // Storage
+  uint256 public gameStart;
   uint256 public playerCount;
   mapping(address => uint8) public playerTeam;
+  mapping(address => uint256) public playerLastMove;
+  mapping(uint16 => uint16[]) public adjacentSpaces;
+  mapping(address => uint16) public playerLocation;
 
   // Events
   event Registered(address indexed player, address nftContract, uint256 nftTokenId);
 
+  // Structs
+  struct Space {
+    uint16 spaceId;
+    uint16[] adjacentSpaces;
+  }
 
-  constructor(address trustedForwarder) EssentialERC2771Context(trustedForwarder) {}
+  constructor(address trustedForwarder) EssentialERC2771Context(trustedForwarder) {
+    // TODO
+    gameStart = block.timestamp;
+  }
+
+  function setMap(Space[] calldata spaces) external onlyOwner {
+    uint256 count = spaces.length;
+    for (uint256 index = 0; index < count; index++) {
+      Space memory space = spaces[index];
+      adjacentSpaces[space.spaceId] = space.adjacentSpaces;
+    }
+  }
 
   function register() external onlyForwarder {
     playerCount += 1;
     uint256 team = playerCount % teamCount;
     playerTeam[_msgSender()] = uint8(team);
+    playerLocation[_msgSender()] = uint16(team);
 
     IForwardRequest.NFT memory nft = _msgNFT();
 
     emit Registered(_msgSender(), nft.contractAddress, nft.tokenId);
+  }
+
+  function currentRoundStart() public view returns (uint256) {
+    return gameStart + (currentRound() - 1) * teamCount * turnLength; 
+  }
+
+  function currentRound() public view returns (uint8) {
+    uint256 elapsed = block.timestamp - gameStart;
+
+    return uint8((elapsed / (teamCount * turnLength)) + 1);
+  }
+
+  function currentTeamMove() public view returns (uint256) {
+    uint256 roundStart = gameStart + (turnLength * teamCount * (currentRound() - 1));
+    uint256 elapsedRound = block.timestamp - roundStart;
+
+    return (elapsedRound / turnLength) + 1;
   }
 
   function playersPerTeam(uint8 team) public view returns (uint256 count){
@@ -32,5 +71,26 @@ contract AnOnchainGame is EssentialERC2771Context {
     uint256 mod = playerCount % teamCount;
     
     count = min + (mod > (team - 1) ? 1 : 0);
+  }
+
+  function performMove(uint16 targetSpace) external onlyForwarder {
+    address player = _msgSender();
+    require(playerTeam[player] == currentTeamMove(), "Not your team's turn");
+    require(playerLastMove[player] < currentRoundStart(), "Move alrready taken this round");
+    
+    uint16 currentSpace = playerLocation[player];
+    uint256 availableSpaceCount = adjacentSpaces[currentSpace].length;
+    
+    bool validMove = false;
+    for (uint256 index = 0; index < availableSpaceCount; index++) {
+      if (adjacentSpaces[currentSpace][index] == targetSpace) {
+        validMove = true;
+        break;
+      }
+    }
+
+    require(validMove == true, "Ivalid Move");
+    playerLastMove[player] = block.timestamp;
+    playerLocation[player] = targetSpace;
   }
 }
--- test/AnOnchainGameTest.t.sol
+++ test/AnOnchainGameTest.t.sol
@@ -10,7 +10,6 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 import "forge-std/console.sol";
 
-
 contract AnOnchainGameTest is Test {
     using ECDSA for bytes32;
 
@@ -20,13 +19,16 @@ contract AnOnchainGameTest is Test {
 
     uint256 internal playerPrivateKey;
     address internal player;
+    uint256 internal nonce;
 
     uint256 internal ownershipSignerPrivateKey;
     address internal ownershipSigner;
+    string[] internal urls;
+
+    AnOnchainGame.Space[] internal spaces;
+    uint16[] adjacentSpaces;
 
     function setUp() public {
-        string[] memory urls;
-        
         forwarder = new EssentialForwarder("EssentialForwarder", urls);
         sigUtils = new SigUtils(forwarder._domainSeparatorV4());
         gameContract = new AnOnchainGame(address(forwarder));
@@ -41,112 +43,220 @@ contract AnOnchainGameTest is Test {
     }
 
     // helper for building request struct
-    function buildRequest(bytes memory selector) 
-        internal 
-        view 
-        returns (IForwardRequest.ERC721ForwardRequest memory) {
-            return IForwardRequest.ERC721ForwardRequest({
-                to: address(gameContract),
-                from: player,
-                authorizer: player,
-                nftContract: player,
-                nonce: 0,
-                nftChainId: block.chainid,
-                nftTokenId: 1,
-                targetChainId: block.chainid,
-                value: 0,
-                gas: 1e6,
-                data: selector
-            });
+    function buildRequest(bytes memory selector) internal returns (IForwardRequest.ERC721ForwardRequest memory req) {
+        req = IForwardRequest.ERC721ForwardRequest({
+            to: address(gameContract),
+            from: player,
+            authorizer: player,
+            nftContract: player,
+            nonce: nonce,
+            nftChainId: block.chainid,
+            nftTokenId: 1,
+            targetChainId: block.chainid,
+            value: 0,
+            gas: 1e6,
+            data: selector
+        });
+
+        nonce += 1;
     }
 
     // helper for signing request struct
-    function signRequest(IForwardRequest.ERC721ForwardRequest memory request) 
-        internal  
-        returns (bytes memory) {
-            bytes32 digest = sigUtils.getTypedDataHash(request);
-            (uint8 v, bytes32 r, bytes32 s) = vm.sign(playerPrivateKey, digest);
-            return abi.encodePacked(r, s, v);
+    function signRequest(IForwardRequest.ERC721ForwardRequest memory request) internal returns (bytes memory) {
+        bytes32 digest = sigUtils.getTypedDataHash(request);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(playerPrivateKey, digest);
+        return abi.encodePacked(r, s, v);
     }
 
     // helper for mocking REST API for signing ownership
-    function mockOwnershipSig(IForwardRequest.ERC721ForwardRequest memory request) 
-        internal  
-        returns (bytes memory) {
-        bytes32 message = forwarder.createMessage(
+    function mockOwnershipSig(IForwardRequest.ERC721ForwardRequest memory request) internal returns (bytes memory) {
+        bytes32 message = forwarder
+            .createMessage(
+                request.from,
+                request.authorizer,
+                request.nonce,
+                request.nftChainId,
+                request.nftContract,
+                request.nftTokenId,
+                block.timestamp
+            )
+            .toEthSignedMessageHash();
+
+        (uint8 vMock, bytes32 rMock, bytes32 sMock) = vm.sign(ownershipSignerPrivateKey, message);
+        return abi.encodePacked(rMock, sMock, vMock);
+    }
+
+    function metaTx(bytes memory encodedFunc) internal {
+        IForwardRequest.ERC721ForwardRequest memory req = buildRequest(encodedFunc);
+
+        bytes memory sig = signRequest(req);
+        bytes memory data = abi.encode(block.timestamp, req, sig);
+        bytes memory response = mockOwnershipSig(req);
+
+        forwarder.executeWithProof(response, data);
+    }
+
+    function setMap() internal {
+        adjacentSpaces.push(2);
+        spaces.push(AnOnchainGame.Space({spaceId: 1, adjacentSpaces: adjacentSpaces}));
+
+        adjacentSpaces[0] = 1;
+        adjacentSpaces.push(3);
+        spaces.push(AnOnchainGame.Space({spaceId: 2, adjacentSpaces: adjacentSpaces}));
+
+        adjacentSpaces[0] = 2;
+        delete (adjacentSpaces[1]);
+        spaces.push(AnOnchainGame.Space({spaceId: 3, adjacentSpaces: adjacentSpaces}));
+
+        gameContract.setMap(spaces);
+    }
+
+    function register() internal {
+        metaTx(abi.encode(keccak256("register()")));
+    }
+
+    // We use EIP-3668 OffchainLookup for trust-minimized cross-chain token gating.
+    // The forwarding contract will revert with an OffchainLookup error in the
+    // "happy path" - the revert is expected and has params we may want to assert
+    function testFailRegister() public {
+        IForwardRequest.ERC721ForwardRequest memory request = buildRequest(abi.encode(keccak256("register()")));
+
+        bytes memory sig = signRequest(request);
+
+        bytes memory callData = abi.encode(
             request.from,
             request.authorizer,
             request.nonce,
             request.nftChainId,
             request.nftContract,
             request.nftTokenId,
+            block.chainid,
             block.timestamp
-        ).toEthSignedMessageHash();
-
-        (uint8 vMock, bytes32 rMock, bytes32 sMock) = vm.sign(ownershipSignerPrivateKey, message);
-        return abi.encodePacked(rMock, sMock, vMock);
-    }
-
-    // We use EIP-3668 OffchainLookup for trust-minimized cross-chain token gating.
-    // The forwarding contract will revert with an OffchainLookup error in the 
-    // "happy path" - the revert is expected and has params we may want to assert
-    function testFailRegister() public {
-        IForwardRequest.ERC721ForwardRequest memory request = buildRequest(
-            abi.encode(keccak256("register()"))
         );
 
-        bytes memory sig = signRequest(request);
-
-        vm.expectRevert(EssentialForwarder.OffchainLookup.selector);
-        
-        forwarder.preflight(request, sig);
+        vm.expectRevert(
+            abi.encodeWithSignature(
+                "OffchainLookup(address,string[],bytes,bytes4,bytes)",
+                player,
+                urls,
+                callData,
+                EssentialForwarder.executeWithProof.selector,
+                abi.encode(block.timestamp, request, sig)
+            )
+        );
+        register();
     }
 
     // If we know that EssentialForwarder#preflight reverts with OffchainLookup,
     // we can unit test EssentialForwarder#executeWithProof by mocking out the
-    // signature that our API would normally provide. 
-    function testTrustedRegister() public {
-        IForwardRequest.ERC721ForwardRequest memory request = buildRequest(
-            abi.encode(keccak256("register()"))
-        );
-
-        bytes memory userSignature = signRequest(request);
-        bytes memory extraData = abi.encode(block.timestamp, request, userSignature);
-        bytes memory response = mockOwnershipSig(request);
-
-        forwarder.executeWithProof(response, extraData);
-    }
-
+    // signature that our API would normally provide.
     function testTrustedRegisterPlayerCount() public {
-        IForwardRequest.ERC721ForwardRequest memory request = buildRequest(
-            abi.encode(keccak256("register()"))
-        );
+        register();
 
-        bytes memory userSignature = signRequest(request);
-        bytes memory extraData = abi.encode(block.timestamp, request, userSignature);
-        bytes memory response = mockOwnershipSig(request);
-
-        forwarder.executeWithProof(response, extraData);
-        
         uint256 playerCount = gameContract.playerCount();
         assertEq(playerCount, 1);
     }
 
     function testTrustedRegisterTeamAssignment() public {
-        IForwardRequest.ERC721ForwardRequest memory request = buildRequest(
-            abi.encode(keccak256("register()"))
-        );
-
-        bytes memory userSignature = signRequest(request);
-        bytes memory extraData = abi.encode(block.timestamp, request, userSignature);
-        bytes memory response = mockOwnershipSig(request);
-
-        forwarder.executeWithProof(response, extraData);
+        register();
 
         uint256 count = gameContract.playersPerTeam(1);
         assertEq(count, 1);
 
         uint8 team = gameContract.playerTeam(player);
         assertEq(team, 1);
+    }
+
+    function testTrustedRegisterLocationAssignment() public {
+        register();
+
+        uint256 spaceId = gameContract.playerLocation(player);
+        assertEq(spaceId, 1);
+    }
+
+    function testGameStart() public {
+        uint256 start = gameContract.gameStart();
+        assertEq(start, block.timestamp);
+    }
+
+    function testCurrentRound() public {
+        uint256 turnLength = gameContract.turnLength();
+        uint256 teamCount = gameContract.teamCount();
+
+        uint256 round = gameContract.currentRound();
+        assertEq(round, 1);
+
+        vm.warp(block.timestamp + (turnLength * teamCount));
+
+        uint256 round2 = gameContract.currentRound();
+        assertEq(round2, 2);
+
+        vm.warp(block.timestamp + (turnLength * teamCount));
+
+        uint256 round3 = gameContract.currentRound();
+        assertEq(round3, 3);
+    }
+
+    function testCurrentTeamMove() public {
+        uint256 team = gameContract.currentTeamMove();
+        assertEq(team, 1);
+
+        uint256 turnLength = gameContract.turnLength();
+        vm.warp(block.timestamp + turnLength);
+
+        uint256 team2 = gameContract.currentTeamMove();
+        assertEq(team2, 2);
+
+        vm.warp(block.timestamp + turnLength);
+
+        uint256 team3 = gameContract.currentTeamMove();
+        assertEq(team3, 3);
+
+        vm.warp(block.timestamp + turnLength);
+
+        uint256 team1 = gameContract.currentTeamMove();
+        assertEq(team1, 1);
+    }
+
+    function testCurrentRoundStart() public {
+        uint256 turnLength = gameContract.turnLength();
+        uint256 teamCount = gameContract.teamCount();
+        uint256 start = gameContract.gameStart();
+
+        uint256 round1Start = gameContract.currentRoundStart();
+        assertEq(round1Start, block.timestamp);
+
+        vm.warp(block.timestamp + (turnLength * teamCount));
+
+        uint256 round2Start = gameContract.currentRoundStart();
+        assertEq(round2Start, start + (turnLength * teamCount));
+    }
+
+    function testSetMap() public {
+        setMap();
+        uint16 oneToTwo = gameContract.adjacentSpaces(1, 0);
+        assertEq(oneToTwo, 2);
+
+        uint16 twoToOne = gameContract.adjacentSpaces(2, 0);
+        assertEq(twoToOne, 1);
+
+        uint16 twoToThree = gameContract.adjacentSpaces(2, 1);
+        assertEq(twoToThree, 3);
+
+        uint16 threeToTwo = gameContract.adjacentSpaces(3, 0);
+        assertEq(threeToTwo, 2);
+    }
+
+    function testTrustedMove() public {
+        setMap();
+        register();
+
+        metaTx(abi.encodeWithSignature("performMove(uint16)", uint16(2)));
+
+        uint256 spaceId = gameContract.playerLocation(player);
+        assertEq(spaceId, 2);
+
+        uint256 lastMove = gameContract.playerLastMove(player);
+        assertEq(lastMove, block.timestamp);
     }
 }
